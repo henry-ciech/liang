61b2e87d430f18f824e3cc9eb0d44dce2f1257c6
Heryk Ciechanowiec <henryk.ciechanowiec@gmail.com>
2022-09-17 21:04:11 +0300

    Add tasks 7_36 and 7_37

--- src/main/java/eu/ciechanowiec/liang/chapter07_tasks/Task_7_36.java
+++ src/main/java/eu/ciechanowiec/liang/chapter07_tasks/Task_7_36.java
@@ -1,4 +1,57 @@
 package eu.ciechanowiec.liang.chapter07_tasks;
 
-class Task_7_36 {
+class Task_7_36{
+
+      public static void main(String[] args) {
+            int number = 8;
+            nQueens(number);
+      }
+
+      private static void nQueens(int numberOfQueens) {
+            int[] board = new int[numberOfQueens];
+            placeQueen(board, 0, numberOfQueens);
+            displayQueens(board);
+      }
+
+      private static void placeQueen(int[] board, int current, int noOfQueens) {
+            for (int i = 0; i < noOfQueens; i++) {
+                  board[current] = i;
+                  if (noKill(board, current)) {
+                        placeQueen(board, current + 1, noOfQueens);
+                        break;
+                  }
+            }
+      }
+
+      private static boolean noKill(int[] board, int currentColumnOfQueen) {
+            for (int i = 0; i < currentColumnOfQueen; i++) {
+                  if (board[i] == board[currentColumnOfQueen]) {
+                        return false;
+                  }
+
+                  if ((currentColumnOfQueen - i) == Math.abs(board[currentColumnOfQueen] - board[i])) {
+                        return false;
+                  }
+            }
+            return true;
+      }
+
+      private static void displayQueens(int[] board) {
+            System.out.print("\n");
+
+            int n = board.length;
+
+            for (int i = 0; i < n; i++) {
+                  for (int value : board) {
+                        if (value == i) {
+                              System.out.print("Q\t");
+                        }
+                        else {
+                              System.out.print("|\t");
+                        }
+                  }
+                  System.out.print("\n");
+            }
+      }
 }
+
--- src/main/java/eu/ciechanowiec/liang/chapter07_tasks/Task_7_37.java
+++ src/main/java/eu/ciechanowiec/liang/chapter07_tasks/Task_7_37.java
@@ -1,4 +1,90 @@
 package eu.ciechanowiec.liang.chapter07_tasks;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+
 class Task_7_37 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] leftOrRight = new String[7];
+
+        System.out.println("Enter the number of balls to drop:");
+        int ballsToDrop = scanner.nextInt();
+
+        System.out.println("Enter the number of slots in the bean machine:");
+        int numberOfSlots = scanner.nextInt();
+        int[] slotCount = new int[numberOfSlots];
+
+        for (int i = 0; i < ballsToDrop; i++) {
+            leftOrRightSimulation(leftOrRight, slotCount);
+            for (String s : leftOrRight) {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+
+        printArray(slotCount, ballsToDrop);
+        System.out.println();
+    }
+
+    private static void leftOrRightSimulation(String[] leftOrRight, int[] slotCount) {
+        RandomGenerator random = new Random();
+        int number;
+        int countR = 0;
+
+        for (int i = 0; i < leftOrRight.length; i++) {
+            number = random.nextInt(3);
+
+            if (number == 0) {
+                leftOrRight[i] = "L";
+            } else {
+                leftOrRight[i] = "R";
+                countR++;
+            }
+        }
+        slotCount[countR]++;
+    }
+
+
+    private static void printArray(int[] slots, int numberOfBalls) {
+
+        while (!isEmpty(slots)) {
+            if (isRowEmpty(slots, numberOfBalls)) {
+                numberOfBalls--;
+                continue;
+            }
+
+            for (int i = 0; i < slots.length; i++) {
+                if (slots[i] >= numberOfBalls) {
+                    System.out.print("0");
+                    slots[i]--;
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            numberOfBalls--;
+            System.out.println("");
+
+        }
+    }
+
+    private static boolean isEmpty(int[] slots) {
+        for (int slot : slots) {
+            if (slot != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isRowEmpty(int[] slots, int rowNum) {
+        for (int slot : slots) {
+            if (slot == rowNum) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
